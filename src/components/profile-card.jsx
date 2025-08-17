6498e390f63c99b25786ca6478eb10797202a8f0
VikasRajput-18 <vikasvikas98809@gmail.com>
2025-08-17 13:40:46 +0530

    interested and ignore profile api integration

--- src/components/profile-card.jsx
+++ src/components/profile-card.jsx
@@ -1,51 +1,56 @@
-
 import {
     Card,
     CardContent,
     CardFooter
 } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
+import { motion } from "framer-motion"
 
-const ProfileCard = ({ profile }) => {
+const ProfileCard = ({ profile, onClick, loading, exitDirection }) => {
     return (
-        <Card className="bg-accent-foreground  text-white border-neutral-600 shadow-2xl p-2 min-w-sm max-w-sm">
-            <CardContent className="p-0">
-                <div className='group w-full '>
-                    <div className='h-72 overflow-hidden rounded-md relative'>
-                        {
-                            profile.gender ?
-                                <p className="absolute  text-white px-4 rounded-full right-2 top-2 capitalize bg-primary z-1">{profile.gender}</p>
-                                : null
-                        }
-
-
-                        <img src={profile?.photoUrl} alt={profile?.firstName} className='w-full object-cover h-72 group-hover:scale-110 transition-all duration-200 ease-in-out rounded-md' />
-                    </div>
-
-                    <div className='mt-2'>
-                        <div className="flex items-center justify-between">
-                            <p className='font-semibold text-xl'>{profile?.firstName} {profile?.lastName}</p>
+        <motion.div
+            key={profile._id}
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={
+                exitDirection === "left"
+                    ? { x: -200, y: 200, opacity: 0, scale: 0.8 } // left bottom
+                    : exitDirection === "right"
+                        ? { x: 200, y: 200, opacity: 0, scale: 0.8 } // right bottom
+                        : {}
+            }
+            transition={{ duration: 0.4 }}
+        >
+            <Card className="bg-accent-foreground text-white border-neutral-600 shadow-2xl p-2 min-w-sm max-w-sm">
+                <CardContent className="p-0">
+                    <div className='group w-full '>
+                        <div className='h-72 overflow-hidden rounded-md relative'>
                             {
-                                profile.age ?
-                                    <p>Age : {profile.age}</p> : null
+                                profile.gender ?
+                                    <p className="absolute text-white px-4 rounded-full right-2 top-2 capitalize bg-primary z-1">{profile.gender}</p>
+                                    : null
                             }
+                            <img src={profile?.photoUrl} alt={profile?.firstName}
+                                className='w-full object-cover h-72 group-hover:scale-110 transition-all duration-200 ease-in-out rounded-md' />
                         </div>
-                    </div>
-                    {
-                        profile.bio ?
-                            <p className='mt-2 line-clamp-2'>{profile.bio}</p>
-                            : null
-                    }
-                </div>
 
-            </CardContent>
-            <CardFooter className="p-0 pb-2">
-                <div className="flex items-center justify-end  w-full gap-x-2">
-                    <Button className="cursor-pointer" variant="destructive">Ignore</Button>
-                    <Button variant="outline" className="text-foreground cursor-pointer">Interested</Button>
-                </div>
-            </CardFooter>
-        </Card>
+                        <div className='mt-2'>
+                            <div className="flex items-center justify-between">
+                                <p className='font-semibold text-xl'>{profile?.firstName} {profile?.lastName}</p>
+                                {profile.age ? <p>Age : {profile.age}</p> : null}
+                            </div>
+                        </div>
+                        {profile.bio ? <p className='mt-2 line-clamp-2'>{profile.bio}</p> : null}
+                    </div>
+                </CardContent>
+                <CardFooter className="p-0 pb-2">
+                    <div className="flex items-center justify-end w-full gap-x-2">
+                        <Button disabled={loading} onClick={() => onClick("ignored", profile._id)} variant="destructive" className="cursor-pointer">Ignore</Button>
+                        <Button disabled={loading} variant="outline" onClick={() => onClick("interested", profile._id)} className="text-foreground cursor-pointer">Interested</Button>
+                    </div>
+                </CardFooter>
+            </Card>
+        </motion.div>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,49 @@
+// Home.jsx
 import React, { useEffect, useState } from 'react'
 import { axiosInstance } from '../axios/interceptor'
 import { useDispatch, useSelector } from "react-redux"
-import { addFeed } from '../store/slices/feedSlice'
+import { addFeed, removeUserFromFeed } from '../store/slices/feedSlice'
 import { ProfileSkeleton } from '../components/profile-skeleton'
 import ProfileCard from '../components/profile-card'
+import toast from 'react-hot-toast'
+import { AnimatePresence } from "framer-motion"
 
 const Home = () => {
     const dispatch = useDispatch();
     const profiles = useSelector(state => state.feed)
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+    const [connectionRequestLoading, setConnectionRequestLoading] = useState(false)
+    const [exitDirection, setExitDirection] = useState(null)
+
+    const sendConnectionRequest = async (status, receiverId) => {
+        try {
+            setConnectionRequestLoading(true)
+            // set direction for animation
+            setExitDirection(status === "ignored" ? "left" : "right")
+
+            const response = await axiosInstance.post(`/api/request/send/${status}/${receiverId}`);
+
+            if (response.status === 201) {
+                toast.success(response.data?.message);
+                setTimeout(() => {
+                    dispatch(removeUserFromFeed(receiverId));
+                    setExitDirection(null)
+                }, 400); // wait for animation before removing
+            }
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.error(error)
+        } finally {
+            setConnectionRequestLoading(false)
+        }
+    }
 
     const getFeed = async () => {
         try {
             setIsLoading(true)
             const response = await axiosInstance.get(`/api/user/feed`)
             if (response.status === 200) {
-                dispatch(addFeed(response.data))
+                dispatch(addFeed(response.data?.users))
             }
         } catch (error) {
             console.error(error)
@@ -36,15 +64,24 @@ const Home = () => {
         )
     }
 
-
     return (
         <section>
             <div className="flex items-center justify-center">
-                {
-                    profiles?.users.slice(0, 1).map((profile) => {
-                        return <ProfileCard key={profile?._id} profile={profile} />
-                    })
-                }
+                <AnimatePresence mode="wait">
+                    {
+                        profiles?.slice(0, 1).map((profile) => {
+                            return (
+                                <ProfileCard
+                                    key={profile?._id}
+                                    profile={profile}
+                                    onClick={sendConnectionRequest}
+                                    loading={connectionRequestLoading}
+                                    exitDirection={exitDirection}
+                                />
+                            )
+                        })
+                    }
+                </AnimatePresence>
             </div>
         </section>
     )
